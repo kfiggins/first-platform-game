f6ffc043e762add6f12b037dcba5409cce9c8f8c
Kyler Figgins <kyler.figgins@gmail.com>
2022-04-09 00:09:05 -0400

    cleaned up game scene

--- src/scenes/game.ts
+++ src/scenes/game.ts
@@ -10,23 +10,22 @@ export class GameScene extends Phaser.Scene {
   }
 
   create() {
+    // Create Map
     const map = this.make.tilemap({ key: "map" });
     const tileset = map.addTilesetImage("magic-cliffs", "tiles");
-    console.log(map);
     const ground = map.createLayer("ground", tileset, 0, 0);
-    map.createLayer("beauty", tileset, 0, 0);
     ground.setCollisionByExclusion(-1, true);
-
-    const objectsLayer = map.getObjectLayer("objects");
+    map.createLayer("beauty", tileset, 0, 0);
 
     this.skeletons = this.physics.add.group({
       classType: Skeleton,
-      // collideWorldBounds: true,
       createCallback: (go) => {
         const skeletonGo = go as Skeleton;
         skeletonGo.setScale(1.3);
       },
     });
+
+    const objectsLayer = map.getObjectLayer("objects");
     objectsLayer.objects.forEach((object) => {
       const { x = 0, y = 0, width, height, name } = object;
       switch (name) {
@@ -34,35 +33,15 @@ export class GameScene extends Phaser.Scene {
           this.player = new Player(this, x, y);
           this.physics.add.collider(this.player, ground);
           break;
-        case "skeleton-spawn":
-          // this.skeleton = new Skeleton(this, x, y, "skeleton");
-          // this.skeleton.setScale(1.3);
-          this.skeletons.get(x, y, "skeleton");
-
-          // skeletons.get(x, y, "skeleton");
-          break;
-        case "skeleton-spawn2":
-          // this.skeleton = new Skeleton(this, x, y, "skeleton");
-          // this.skeleton.setScale(1.3);
-          this.skeletons.get(x, y, "skeleton");
-
-          break;
-        case "skeleton-spawn3":
-          // this.skeleton = new Skeleton(this, x, y, "skeleton");
-          // this.skeleton.setScale(1.3);
-          this.skeletons.get(x, y, "skeleton");
-
-          break;
-        case "skeleton-spawn4":
-          // this.skeleton = new Skeleton(this, x, y, "skeleton");
-          // this.skeleton.setScale(1.3);
-          this.skeletons.get(x, y, "skeleton");
-
-          break;
       }
     });
 
-    // this.physics.add.collider(this.skeletons, this.player);
+    const skeletonsObjects = map.getObjectLayer("skeletons");
+    skeletonsObjects.objects.forEach((object) => {
+      const { x = 0, y = 0, width, height, name } = object;
+      this.skeletons.get(x, y, "skeleton");
+    });
+
     this.physics.add.collider(
       this.player,
       this.skeletons,
@@ -76,12 +55,7 @@ export class GameScene extends Phaser.Scene {
     this.cameras.main.startFollow(this.player);
   }
 
-  private handlePlayerSkeletonCollision(
-    player: Phaser.GameObjects.GameObject,
-    skeleton: Phaser.GameObjects.GameObject
-  ) {
-    console.log("collision");
-    console.log(skeleton);
+  private handlePlayerSkeletonCollision(player: Phaser.GameObjects.GameObject, skeleton: any) {
     const dx = this.player.x - skeleton.x;
     const dy = this.player.y - skeleton.y;
     const dir = new Phaser.Math.Vector2(dx, dy).normalize().scale(500);
